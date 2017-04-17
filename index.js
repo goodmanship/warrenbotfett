d2094c640df10ac4ea3ee7746e89490e7fb1186c
rio <riogoodman@gmail.com>
2017-04-16 23:03:51 -0700

    order book sync on page

--- index.js
+++ index.js
@@ -1,20 +1,69 @@
 const config = require("./config")
-const gdax = require('gdax');
-var websocket = new gdax.WebsocketClient(['ETH-USD']);
-const pc = new gdax.PublicClient('ETH-USD')
 
-const mc = require('mongodb').MongoClient
-let app = {}
+const express = require('express')
+const gdax = require('gdax')
+const orderbookSync = new gdax.OrderbookSync(['ETH-USD'])
+
+const app = express()
+
+// const ws = new gdax.WebsocketClient(['ETH-USD']);
+// const pc = new gdax.PublicClient('ETH-USD')
+// const mc = require('mongodb').MongoClient
+
+// const Bullish = require('technicalindicators').Bullish
+// const Bearish = require('technicalindicators').Bearish
+
+function sellSide(data) {
+  return data['side'] == 'sell'
+}
+
+function buySide(data) {
+  return data['side'] == 'buy'
+}
+
+function getOrders(limit=100) {
+  const book = orderbookSync.book.state()
+  const asks = book.asks.slice(0, limit - 1)
+  const bids = book.bids.slice(0, limit - 1)
+  return asks.concat(bids)
+}
+// mongo connection
 // let collection = db.collection('documents')
-mc.connect(config.mongoUrl, (err, db) => {
-  if (err) throw(err)
-  app.db = db
-})
+// mc.connect(config.mongoUrl, (err, db) => {
+//   if (err) throw(err)
+//   app.db = db
+// })
 
 // this will put every message into the db
-websocket.on('message', data => {
-  col = app.db.collection('messages')
-  col.insert(data, (err, r) => {
-    if (err) throw(err)
-  })
-});
+// pc.getProductOrderBook((err, r, data) => {
+//   console.log(data)
+// })
+
+// synced order book
+// const orderbookSync = new gdax.OrderbookSync()
+// setInterval(() => console.log(orderbookSync.book.state()), 1000)
+
+// ws.on('message', data => {
+//   console.log(data)
+  // col = app.db.collection('messages')
+  // col.insert(data, (err, r) => {
+  //   if (err) throw(err)
+
+// Node config
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+
+// Routing
+app.get('/', (req, res) => {
+  res.render('trade')
+})
+
+app.get('/orderbook', (req, res) => {
+  res.json(getOrders())
+})
+
+// start node server
+app.listen(3000, function () {
+  console.log('Warning: dangerously high ROI detected')
+})
+
---
+++ public/js/trade.js
@@ -0,0 +1,25 @@
+console.log("i got this here")
+const endpoint = {
+  'orderbook': '/orderbook'
+}
+const buyOrderBook = document.querySelector('#buyOrders')
+const sellOrderBook = document.querySelector('#sellOrders')
+
+function sellSort(x, y) {
+  if (y.price < x.price) return -1
+  if (y.price == x.price) return 0
+  if (y.price > x.price) return 1
+}
+
+function showBook(data) {
+  const buyOrderList = data.filter(order => order['side'] == 'buy').map(order => `<li class='${order['side']}'><span><strong>${parseFloat(order['price']).toFixed(2)}</strong> ${order['size']}</span></li>`).join('')
+  const sellOrderList = data.filter(order => order['side'] == 'sell').sort((a,b) => parseFloat(b['price']) - parseFloat(a['price'])).map(order => `<li class='${order['side']}'><span><strong>${parseFloat(order['price']).toFixed(2)}</strong> ${order['size']}</span></li>`).join('')
+  buyOrderBook.innerHTML = buyOrderList
+  sellOrderBook.innerHTML = sellOrderList
+}
+
+setInterval(() => {
+  fetch(endpoint['orderbook'])
+    .then(blob => blob.json())
+    .then(data => showBook(data))
+}, 1000)
