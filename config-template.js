6d0753904c118c90ae0a81255e56420de3be8ae2
rio <riogoodman@gmail.com>
2017-04-21 13:01:37 -0700

    template of gitignored config file

---
+++ config-template.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiPassphrase: 'x',
+  apiKey: 'x',
+  apiSecret: 'x',
+  apiUrl: 'https://api-public.sandbox.gdax.com',
+  wsUrl: 'wss://ws-feed-public.sandbox.gdax.com',
+  defaultProduct: 'ETH-USD',
+  mongoUrl: 'mongodb://localhost:27017/db',
+  slackUrl: 'x'
+}
