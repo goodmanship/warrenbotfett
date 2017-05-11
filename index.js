f150fd2eedf4fdfe3a0e87018ee12c3a8d286a8a
rio <riogoodman@gmail.com>
2017-05-11 12:24:26 -0700

    linted

--- index.js
+++ index.js
@@ -1,156 +1,107 @@
-const config = require("./lib/config")
-const slack = require('./lib/slack')
-
+const Afplay = require('afplay')
 const express = require('express')
 const gdax = require('gdax')
-const orderbookSync = new gdax.OrderbookSync(['ETH-USD'])
 
-const Afplay = require('afplay')
-const player = new Afplay
+const config = require('./lib/config')
+const slack = require('./lib/slack')
 
 const app = express()
 app.orders = []
-
-const ws = new gdax.WebsocketClient(['ETH-USD']);
-// const pc = new gdax.PublicClient('ETH-USD')
-// const mc = require('mongodb').MongoClient
-
-// Defaults to https://api.gdax.com if apiURI omitted
 const authedClient = new gdax.AuthenticatedClient(
   config.apiKey,
   config.apiSecret,
   config.apiPassphrase,
   config.apiURI
 )
-authedClient.getAccounts((err, r, data) => console.log(data))
+const logger = config.logger
+const orderbookSync = new gdax.OrderbookSync(['ETH-USD'])
+const player = new Afplay()
+const ws = new gdax.WebsocketClient(['ETH-USD'])
+
+authedClient.getAccounts((err, r, data) => logger.info(data))
+
+// const pc = new gdax.PublicClient('ETH-USD')
+// const mc = require('mongodb').MongoClient
+// Defaults to https://api.gdax.com if apiURI omitted
 
 function filledOrder(trade) {
   authedClient.getOrders((err, r, data) => {
     // if the order was filled, it wont be in the order list any more
-    const newOrders = data
-    data = app.orders
-    app.orders = newOrders
+    const oldOrders = app.orders
+    app.orders = data
 
-    if (data == null) return false
-    const o = data.find(order => order.id == trade.maker_order_id)
+    if (oldOrders == null) return false
+    const o = oldOrders.find(order => order.id === trade.maker_order_id)
     if (o !== undefined) {
       slack.reportFill(trade, o)
-      player.play('./public/sounds/kick.wav', {volume: 4})
+      player.play('./public/sounds/kick.wav', { volume: 4 })
+      return true
     }
+    return false
   })
 }
 
-function sellSide(data) {
-  return data['side'] == 'sell'
-}
-
-function buySide(data) {
-  return data['side'] == 'buy'
-}
-
-function getOrders(limit=100) {
+function getOrders(limit = 100) {
   const book = orderbookSync.book.state()
   const asks = book.asks.slice(0, limit - 1)
   const bids = book.bids.slice(0, limit - 1)
   return asks.concat(bids)
 }
 
-function soundVol(size) {
-  let x = parseFloat(size)
-  if (x <= 5)
-    return 0.5
-  else
-    return Math.min((x/8).toFixed(2), 1)
-}
-
-function playAnotherTen() {
-  let sum = 0
-  for (trade in app.trades.slice(0,10)) {
-    if (trade.side == 'sell') {
-      sum -= trade.size
-    } else {
-      sum += trade.size
-    }
-  }
-  app.trades = app.trades.slice(9)
-  let side = sum < 0 ? 'sell' : 'buy'
-  app.trades.unshift({side, size: sum})
-  playAnotherOne()
-}
-
-function playAnotherOne() {
-  let sound
-  let vol
-  let time
-  let data = app.trades.shift()
-  if (data.side == 'sell') {
-    if (data.size >= 1000) {
-      sound = './public/sounds/Glass.aiff'
-      vol = 5
-      time = 0.5
-    } else if (data.size >= 100) {
-      sound = './public/sounds/Morse.aiff'
-      vol = 3
-      time = 0.2
-    } else if (data.size >= 10) {
-      sound = './public/sounds/Frog.aiff'
-      vol = 2
-      time = 0.1
-    } else {
-      sound = './public/sounds/Tink.aiff'
-      vol = 1
-      time = 0.1
-    }
-  } else {
-    if (data.size >= 1000) {
-      sound = './public/sounds/Basso.aiff'
-      vol = 5
-      time = 0.5
-    } else if (data.size >= 100) {
-      sound = './public/sounds/Bottle.aiff'
-      vol = 3
-      time = 0.2
-    } else if (data.size >= 10) {
-      sound = './public/sounds/Ping.aiff'
-      vol = 2
-      time = 0.1
-    } else {
-      sound = './public/sounds/Pop.aiff'
-      vol = 1
-      time = 0.1
-    }
-  }
-  app.audio = player.play(sound, {volume: vol, time: time})
-  setTimeout(audioPoll, 50)
-}
-
 function audioPoll() {
   setTimeout(() => {
-    // if (app.trades.length > 10) playAnotherTen()
-    if (app.trades.length > 0) playAnotherOne()
-    else setTimeout(audioPoll, 100)
+    if (app.trades.length > 0) {
+      let sound
+      let volume
+      let time
+      const data = app.trades.shift()
+      if (data.side === 'sell') {
+        if (data.size >= 1000) {
+          sound = './public/sounds/Glass.aiff'
+          volume = 5
+          time = 0.5
+        } else if (data.size >= 100) {
+          sound = './public/sounds/Morse.aiff'
+          volume = 3
+          time = 0.2
+        } else if (data.size >= 10) {
+          sound = './public/sounds/Frog.aiff'
+          volume = 2
+          time = 0.1
+        } else {
+          sound = './public/sounds/Tink.aiff'
+          volume = 1
+          time = 0.1
+        }
+      } else if (data.size >= 1000) {
+        sound = './public/sounds/Basso.aiff'
+        volume = 5
+        time = 0.5
+      } else if (data.size >= 100) {
+        sound = './public/sounds/Bottle.aiff'
+        volume = 3
+        time = 0.2
+      } else if (data.size >= 10) {
+        sound = './public/sounds/Ping.aiff'
+        volume = 2
+        time = 0.1
+      } else {
+        sound = './public/sounds/Pop.aiff'
+        volume = 1
+        time = 0.1
+      }
+      app.audio = player.play(sound, { volume, time })
+      setTimeout(audioPoll, 50)
+    } else {
+      setTimeout(audioPoll, 100)
+    }
   }, 5)
 }
-// mongo connection
-// let collection = db.collection('documents')
-// mc.connect(config.mongoUrl, (err, db) => {
-//   if (err) throw(err)
-//   app.db = db
-// })
 
-// this will put every message into the db
-// pc.getProductOrderBook((err, r, data) => {
-//   console.log(data)
-// })
-
-// synced order book
-// const orderbookSync = new gdax.OrderbookSync()
-// setInterval(() => console.log(orderbookSync.book.state()), 1000)
-
-app.audio = player.play('./public/sounds/Submarine.aiff', {volume: 1, time: 0.1}).then(audioPoll)
+app.audio = player.play('./public/sounds/Submarine.aiff', { volume: 1, time: 0.1 }).then(audioPoll)
 app.trades = []
-ws.on('message', data => {
-  if (data.type == 'match') {
+ws.on('message', (data) => {
+  if (data.type === 'match') {
     app.trades.push(data)
     filledOrder(data)
   }
@@ -171,12 +122,11 @@ app.get('/orderbook', (req, res) => {
 
 app.post('/allorder', (req, res) => {
   const body = req.body
-  console.log(body)
+  logger.info(body)
   res.sendStatus(200)
 })
 
 // start node server
-app.listen(3000, function () {
-  console.log('Warning: dangerously high ROI detected')
+app.listen(3000, () => {
+  logger.info('Warning: dangerously high ROI detected')
 })
-
--- public/js/trade.js
+++ public/js/trade.js
@@ -1,28 +1,42 @@
-console.log("i got this here")
 const endpoint = {
-  'orderbook': '/orderbook',
-  'allorder': '/allorder'
-}
-const buyOrderBook = document.querySelector('#buyOrders')
-const sellOrderBook = document.querySelector('#sellOrders')
-const title = document.querySelector('#mainTitle')
-
-function sellSort(x, y) {
-  if (y.price < x.price) return -1
-  if (y.price == x.price) return 0
-  if (y.price > x.price) return 1
+  orderbook: '/orderbook',
+  allorder: '/allorder'
 }
 
-function showBook(data) {
-  const buyOrderList = data.filter(order => order['side'] == 'buy').map(order => `<li class='${order['side']}'><span><strong>${parseFloat(order['price']).toFixed(2)}</strong> ${order['size']}</span></li>`).join('')
-  const sellOrderList = data.filter(order => order['side'] == 'sell').sort((a,b) => parseFloat(b['price']) - parseFloat(a['price'])).map(order => `<li class='${order['side']}'><span><strong>${parseFloat(order['price']).toFixed(2)}</strong> ${order['size']}</span></li>`).join('')
-  buyOrderBook.innerHTML = buyOrderList
-  sellOrderBook.innerHTML = sellOrderList
-}
+// const buyOrderBook = document.querySelector('#buyOrders')
+// const sellOrderBook = document.querySelector('#sellOrders')
+
+// function sellSort(x, y) {
+//   if (y.price < x.price) return -1
+//   if (y.price === x.price) return 0
+//   if (y.price > x.price) return 1
+// }
+
+// function showBook(data) {
+//   const buyOrderList = data
+//     .filter(order => order.side === 'buy')
+//     .map((order) => {
+//       `<li class='${order.side}'>
+//         <span><strong>${parseFloat(order.price).toFixed(2)}</strong> ${order.size}</span>
+//       </li>`
+//     })
+//     .join('')
+//   const sellOrderList = data
+//     .filter(order => order.side === 'sell')
+//     .sort((a, b) => parseFloat(b.price) - parseFloat(a.price))
+//     .map((order) => {
+//       `<li class='${order.side}'>
+//         <span><strong>${parseFloat(order.price).toFixed(2)}</strong> ${order.size}</span>
+//       </li>`
+//     })
+//     .join('')
+//   buyOrderBook.innerHTML = buyOrderList
+//   sellOrderBook.innerHTML = sellOrderList
+// }
 
 function placeOrder(side, size, price) {
   fetch(
-    endpoint['allorder'],
+    endpoint.allorder,
     {
       method: 'POST',
       body: { side, size, price }
